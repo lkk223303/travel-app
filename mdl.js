be464f979593d4e3a83599dbb0bdb351feb145d7
Kent Huang <lkk223303@gmail.com>
2021-12-14 20:58:38 +0800

    to ch7 finish

--- mdl.js
+++ mdl.js
@@ -5,9 +5,19 @@ var fortune = require("./lib/fortune");
 // 設定handlebars view 引擎
 var handlebars = require("express3-handlebars").create({
   defaultLayout: "main",
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    },
+  },
 });
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
+
+// 預設情況下，開發模式的視圖緩存停用，產品模式下會啟用，自行啟用視圖緩存⬇️
+// app.set("view cache", true);
 
 app.set("port", process.env.PORT || 3000);
 
@@ -17,7 +27,42 @@ app.use(function (req, res, next) {
   next();
 });
 
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: "Portland",
+        forecastUrl: "http://www.wunderground.com/US/OR/Portland.html",
+        iconUrl: "http://icons-ak.wxug.com/i/c/k/cloudy.gif",
+        weather: "Overcast",
+        temp: "54.1 F (12.3 C)",
+      },
+      {
+        name: "Bend",
+        forecastUrl: "http://www.wunderground.com/US/OR/Bend.html",
+        iconUrl: "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif",
+        weather: "Partly Cloudy",
+        temp: "55.0 F (12.8 C)",
+      },
+      {
+        name: "Manzanita",
+        forecastUrl: "http://www.wunderground.com/US/OR/Manzanita.html",
+        iconUrl: "http://icons-ak.wxug.com/i/c/k/rain.gif",
+        weather: "Light Rain",
+        temp: "55.0 F (12.8 C)",
+      },
+    ],
+  };
+}
+
 app.use(express.static(__dirname + "/public"));
+app.use(require("body-parser")());
+
+app.use(function (req, res, next) {
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weather = getWeatherData();
+  next();
+});
 
 app.get("/", function (req, res, next) {
   res.render("home");
@@ -39,6 +84,45 @@ app.get("/tours/oregon-coast", function (req, res) {
 app.get("/tours/request-group-rate", function (req, res) {
   res.render("tours/request-group-rate");
 });
+app.get("/jquerytest", function (req, res) {
+  res.render("jquery-test");
+});
+app.get("/nursery-rhyme", function (req, res) {
+  res.render("nursery-rhyme");
+});
+app.get("/data/nursery-rhyme", function (req, res) {
+  res.json({
+    animal: "squirrel",
+    bodyPart: "tail",
+    adjective: "bushy",
+    noun: "heck",
+  });
+});
+
+app.post("/process", function (req, res) {
+  console.log(req.body);
+});
+
+//請求標頭 請求物件的headers特性，內含瀏覽器傳送的資訊
+app.get("/headers", function (req, res) {
+  res.set("Content-Type", "text/plain");
+  var s = "";
+  for (var name in req.headers) s += name + " : " + req.headers[name] + "\n";
+  res.send(s);
+});
+
+// // Format
+// app.get("/format", function (req, res) {
+//   res.format({
+//     "text/html": function () {
+//       res.send("<p>hey</p>");
+//     },
+
+//     "appliation/json": function () {
+//       res.send({ message: "hey" });
+//     },
+//   });
+// });
 
 // 404 全部抓取處理程式（中介軟體）
 app.use(function (req, res, next) {
